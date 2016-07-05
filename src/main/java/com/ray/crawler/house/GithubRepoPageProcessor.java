7626e5e81be6c7f9a3f3da3a487123de949e1394
Wu XinYong <sinron.wu@gmail.com>
2016-07-05 22:37:29 +0800

    init project

---
+++ src/main/java/com/ray/crawler/house/GithubRepoPageProcessor.java
@@ -0,0 +1,49 @@
+package com.ray.crawler.house;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * @Copyright (c) 2015, hunantv.com All Rights Reserved.
+ * @author: wuxinyong@e.hunantv.com
+ * @date: 16-7-4
+ * @time: 下午10:36
+ */
+public class GithubRepoPageProcessor implements PageProcessor {
+
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
+
+
+    @Override
+    public void process(Page page) {
+        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
+        page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
+        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+        if (page.getResultItems().get("name") == null) {
+            //skip this page
+            page.setSkip(true);
+        }
+        page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
+//        System.out.println(page.getResultItems().get("author"));
+//        System.out.println(page.getResultItems().get("name"));
+//        System.out.println(page.getResultItems().get("readme"));
+
+    }
+
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft")
+                .addPipeline(new JsonFilePipeline("/Users/wuxinyong/myworkspace/webmagic_test"))
+                .thread(5)
+                .run();
+    }
+}
